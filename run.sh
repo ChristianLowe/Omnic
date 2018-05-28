ae967c704cd3ae08d0676f5a675667417f1ce36c
Christian Lowe <christianlowe.cl@gmail.com>
2018-05-27 21:17:08 -0700

    Merge build.sh into run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
-chmod +x build.sh
-./build.sh
-
-docker run -d --env OMNIC_KEY omnic:latest
+mvn clean compile assembly:single && docker build -t omnic . && docker run -d --env OMNIC_KEY omnic:latest
